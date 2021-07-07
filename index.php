5c14130fa0aa5bab9f8d7dac9e0e1ec9467bb8b9
nithinjayict <nithin.jay@ictgroup.ind.in>
2021-07-07 21:03:30 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+
+method to create a directory in local and pull to git
+
+
+git init
+git status
+
+git add index.php - add file index.php
+git rm --cached index.php - remove file index.php
+git rm -f index.php = force remove file index.php
+
+git commit -m "initial commit"
+git status
+
+git branch -a = listing branch
+git checkout -b nithin - create a branch
+git checkout master - change the active branch
+git branch -d nithin 
+
+git remote add origin git@github.com:nithinjayict/GitTutorials.git
+git push origin master - push files to the master branch
+
+--------------------------------------------------
+
+git clone https://github.com/nithinjayict/GitTutorials.git
+
+
+
